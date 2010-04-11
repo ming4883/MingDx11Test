983a72ac1bdc880c4d30ce61d74ff832d02c863b
unknown <ming@.(none)>
2010-04-11 10:07:01 +0800

    added Histogram.Update.*.hlsl

--- Media/Example02/Histogram.Update.PS.hlsl
+++ Media/Example02/Histogram.Update.PS.hlsl
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------
-// File: Post.DOF.hlsl
+// File: Histogram.Update.PS.hlsl
 //
 // The pixel shader file for the BasicHLSL11 sample.  
 // 
@@ -9,11 +9,9 @@
 //--------------------------------------------------------------------------------------
 // Globals
 //--------------------------------------------------------------------------------------
-cbuffer cbPostCommon : register( b0 )
+cbuffer cbHistogramUpdate : register( b0 )
 {
-	matrix g_InvViewProjScaleBias	: packoffset(c0);
-	float4 g_ZParams				: packoffset(c4);
-	float4 g_HDRParams				: packoffset(c5);
+	float4 g_vInputParams	: packoffset(c0);	// white target, bloom threshold
 };
 
 //--------------------------------------------------------------------------------------
@@ -30,20 +28,7 @@ SamplerState g_samLinear : register( s0 );
 //--------------------------------------------------------------------------------------
 // Textures and Samplers
 //--------------------------------------------------------------------------------------
-Texture2D g_txDepth : register( t0 );
-
-float LinearDepth(float zbufDepth)
-{
-	// http://www.humus.name/index.php?page=Comments&ID=256
-	return 1 / (zbufDepth * g_ZParams.x + g_ZParams.y);
-}
-
-float4 ScreenToWorldPosition(float4 screenPos)
-{
-	// http://www.humus.name/index.php?page=Comments&ID=256
-	float4 wPos = mul(screenPos, g_InvViewProjScaleBias);
-	return float4(wPos.xyz / wPos.w, 1);
-}
+Texture2D g_txHistogram : register( t0 );
 
 //--------------------------------------------------------------------------------------
 // Pixel Shader
@@ -52,20 +37,14 @@ float4 ScreenToWorldPosition(float4 screenPos)
 float4 Main( PS_INPUT Input ) : SV_TARGET
 {
 	int iWDepth, iHDepth;
-	g_txDepth.GetDimensions(iWDepth, iHDepth);
+	g_txHistogram.GetDimensions(iWDepth, iHDepth);
 	
 	// use the depth-value in the screen center as the focus distance
 	float fDepthFocus = 0;
 	int x = iWDepth/2;
 	int y = iHDepth/2;
-	/*
-	fDepthFocus += LinearDepth(g_txDepth.Load(int3(x  , y, 0)).r);
-	fDepthFocus += LinearDepth(g_txDepth.Load(int3(x+1, y, 0)).r);
-	fDepthFocus += LinearDepth(g_txDepth.Load(int3(x  , y+1, 0)).r);
-	fDepthFocus += LinearDepth(g_txDepth.Load(int3(x+1, y+1, 0)).r);
-	return fDepthFocus / 4;
-	*/
-	fDepthFocus = g_txDepth.Load(int3(x, y, 0)).x;
+	
+	fDepthFocus = g_txHistogram.Load(int3(x, y, 0)).x;
 	float3 fPosFocus = ScreenToWorldPosition(float4(x, y, fDepthFocus, 1)).xyz;
 	
 	return float4(fPosFocus, fDepthFocus);
--- Media/Example02/Histogram.Update.VS.hlsl
+++ Media/Example02/Histogram.Update.VS.hlsl
@@ -1,15 +1,14 @@
 //--------------------------------------------------------------------------------------
-// File: Histogram.VS.hlsl
+// File: Histogram.Update.VS.hlsl
 //
-// The shader file for the MingDx11Test.  
+// The vertex shader file for the BasicHLSL11 sample.  
 // 
-// Copyright (c) Chan Ka Ming. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 //--------------------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------------------
 // Globals
 //--------------------------------------------------------------------------------------
-
 
 //--------------------------------------------------------------------------------------
 // Input / Output structures
@@ -17,23 +16,24 @@
 struct VS_INPUT
 {
 	float4 vPosition	: POSITION;
-	uint iInstanceId	: SV_INSTANCEID;
 };
 
 struct VS_OUTPUT
 {
-	float4 vPosition	: SV_POSITION;
+	float4 vPosition : SV_POSITION;
+	float2 vTexcoord : TEXCOORD0;
 };
 
 //--------------------------------------------------------------------------------------
 // Vertex Shader
 //--------------------------------------------------------------------------------------
-VS_OUTPUT Main(VS_INPUT Input)
+VS_OUTPUT Main( VS_INPUT Input )
 {
 	VS_OUTPUT Output;
 	
-	Output.vPosition.x = Input.iInstanceId;
-	Output.vPosition.yzw = 0;
+	Output.vPosition = Input.vPosition;
+	Output.vTexcoord = Input.vPosition.xy * float2(1,-1) * 0.5 + 0.5;
 	
 	return Output;
 }
+
